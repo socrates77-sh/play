583636128fb1bb64daeaa8b372d5886d6c17b2dc
socrates77 <zhwenrong@sina.com>
2017-02-20 17:45:11 +0800

    dev ydzz.py @office
    Signed-off-by: socrates77 <zhwenrong@sina.com>

---
+++ ydzz.py
@@ -0,0 +1,48 @@
+'''
+处理《伊达政宗》电子书
+'''
+__author__ = 'zwr'
+
+import os
+import sys
+
+
+def do_format(filename):
+    if not os.path.exists(filename):
+        print('File %s not existed.' % filename)
+        sys.exit(1)
+
+    # abs_filename = os.path.abspath(filename)
+    # dir_name = os.path.split(abs_filename)[0]
+    prefix = os.path.splitext(filename)[0]
+    ext = os.path.splitext(filename)[1]
+    # print(dir_name, prefix, ext)
+
+    l_lines = ''
+    try:
+        with open(filename, 'r', encoding='utf-16', errors='ignore') as f:
+            l_lines = f.readlines()
+    except Exception as e:
+        print(e)
+
+    # print(l_lines)
+    # for l in l_lines:
+    # print(l)
+
+    out_file = '%s%d%s' % (prefix, 0, ext)
+
+    with open(out_file, 'w+') as f:
+        for l in l_lines:
+            try:
+                f.write(l)
+            except Exception as e:
+                print(e)
+
+    print('[Save]', out_file)
+
+def main():
+    do_format(r'e:\download\ydzz.txt')
+
+
+if __name__ == '__main__':
+    main()
