76ad5c3ad3dbf22cd9c7fe078a8d4f5968d38be1
socrates77 <zhwenrong@sina.com>
2018-07-01 22:40:51 +0800

    dev xmly.py @home
    Signed-off-by: socrates77 <zhwenrong@sina.com>

---
+++ xmly.py
@@ -0,0 +1,123 @@
+import re
+import requests
+import json
+import sys
+import io
+import os
+
+VERSION = '0.1'
+
+URL_TRACK = 'http://www.ximalaya.com/revision/play/tracks?trackIds='
+# m4a_url = 'http://audio.xmcdn.com/group36/M03/14/33/wKgJTVs1ra7CRKMTAISxdTec9wM851.m4a'
+# album_url = 'http://www.ximalaya.com/lishi/3703879/p3/'
+# album_url = 'http://www.ximalaya.com/shangye/269179/'
+
+
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36'
+}
+
+ERR_WEB_ACCESS_FAIL = 'Cannot access web'
+ERR_WEB_EXTRACT_FAIL = 'Cannot extract web'
+
+
+def print_version(version):
+    print('=' * 70)
+    print('%s version: %s' % (os.path.basename(__file__), VERSION))
+    print('=' * 70)
+
+
+def usage():
+    print('usage:')
+    print('    %s xmly_album_url' % os.path.basename(__file__))
+
+
+def remove_duplicate(input_list):
+    return list(set(input_list))
+
+
+class XmldAlbum():
+
+    def __init__(self, album_url):
+        self.__track_list = []
+        self.__count_of_tracks = 0
+        self.__extract_tracks(album_url)
+
+    @property
+    def count_of_tracks(self):
+        return self.__count_of_tracks
+
+    @property
+    def track_list(self):
+        return self.__track_list
+
+    def __extract_tracks(self, album_url):
+        res = requests.get(album_url, headers=HEADERS)
+        if res.status_code == 200:
+            html_text = res.text
+            p = re.compile(
+                '{"index":(\d+?),"trackId":(\d+?),"isPaid":false,"tag":0,"title":"(.+?)","playCount"', re.S)
+            result = re.findall(p, html_text)
+            if(result != []):
+                self.__track_list = remove_duplicate(result)
+                self.__count_of_tracks = len(self.track_list)
+                return True
+            else:
+                print('Error: %s %s' % (ERR_WEB_EXTRACT_FAIL, album_url))
+                return False
+        else:
+            print('Error: %s %s' % (ERR_WEB_ACCESS_FAIL, album_url))
+            return False
+
+
+def save_m4a(url, file_name):
+    res = requests.get(url, timeout=60)
+    size_of_file = open(file_name, 'wb').write(res.content)
+    print('[save] %s (%d bytes)' % (file_name, size_of_file))
+
+
+def download_track(track):
+    index = track[0]
+    track_id = track[1]
+    title = track[2].strip()
+
+    url = URL_TRACK + track_id
+    res = requests.get(url, headers=HEADERS)
+
+    if res.status_code == 200:
+        try:
+            response_json = res.json()
+            m4a_url = response_json['data']['tracksForAudioPlay'][0]['src']
+            # print(m4a_url)
+        except Exception as e:
+            print('Error: %s %s' % (ERR_WEB_EXTRACT_FAIL, url))
+            return False
+        file_name = '%03d-%s.m4a' % (int(index), title)
+        save_m4a(m4a_url, file_name)
+
+    else:
+        print('Error: %s %s' % (ERR_WEB_ACCESS_FAIL, url))
+        return False
+
+
+def main():
+    # sys.stdout = io.TextIOWrapper(sys.stdout, encoding='gb18030')
+
+    print_version(VERSION)
+    if len(sys.argv) != 2:
+        usage()
+        sys.exit(1)
+
+    album_url = sys.argv[1]
+    album = XmldAlbum(album_url)
+
+    for track in album.track_list:
+        # print(track)
+        download_track(track)
+
+    print('=' * 70)
+    print('%d files saved' % album.count_of_tracks)
+
+
+if __name__ == '__main__':
+    main()
